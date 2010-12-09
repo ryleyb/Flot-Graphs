e3ea44b7fa79b6edafa3dd28b95e44fea1776de5
ryleyb <ryleyb@gmail.com>
2010-12-09 15:38:08 -0800

    Reuse the canvas element when plot is called
    See this bug for details:
    http://code.google.com/p/flot/issues/detail?id=269
    
    Essentially, someone noticed that if call plot repeatedly, memory
    usage climbs quickly in most browsers.  Test cases are provided.
    With this change, you can run some of the test cases given for days
    without memory usage changing.
    
    Make sure you do NOT use AJAX when testing this, as jQuery has its
    own set of memory leaks...

--- jquery.flot.js
+++ jquery.flot.js
@@ -711,7 +711,21 @@
             
             canvasWidth = placeholder.width();
             canvasHeight = placeholder.height();
-            placeholder.html(""); // clear placeholder
+            var checkCanvas = placeholder.children('canvas');
+            if (checkCanvas.length && 
+                (checkCanvas.get(0).width == canvasWidth) && 
+                (checkCanvas.get(0).height == canvasHeight)){
+                canvas = checkCanvas.get(0);
+                ctx = canvas.getContext('2d');
+                overlay = checkCanvas.get(1);
+                octx = overlay.getContext('2d');
+                
+                //manually clear canvas
+                ctx.clearRect(0,0,canvasWidth,canvasHeight);
+                octx.clearRect(0,0,canvasWidth,canvasHeight);
+                unbindEvents();
+            } else {
+            placeholder.get(0).innerHTML = ''; //this helps with leaky memory possibly 
             if (placeholder.css("position") == 'static')
                 placeholder.css("position", "relative"); // for positioning labels and overlay
 
@@ -728,6 +742,8 @@
             // overlay canvas for interactive features
             overlay = $(makeCanvas(canvasWidth, canvasHeight)).css({ position: 'absolute', left: 0, top: 0 }).appendTo(placeholder).get(0);
             octx = overlay.getContext("2d");
+            }
+
             octx.stroke();
         }
 
@@ -925,6 +941,13 @@
             axis.tickLength = tickLength;
             axis.box.padding = padding;
             axis.innermost = innermost;
+        }
+
+        function unbindEvents() {
+            // we include the canvas in the event holder too, because IE 7
+            // sometimes has trouble with the stacking order
+            eventHolder = $([overlay, canvas]);
+            eventHolder.unbind();
         }
 
         function fixupAxisBox(axis) {
