141d5c00dda509f66721e5b920e16589fb115547
ryleyb <ryleyb@gmail.com>
2011-01-18 12:08:35 -0800

    Add a playground to dev, thanks to Fabien Menager
    This is code from Fabien Menager's flotr project, which he graciously
    has allowed us to borrow.  I have converted most of it to jQuery, and
    removed whatever we couldn't use.  There are still tons of flot plugins
    and options to be added in, but this is the barebones of it.
    
    Lots of help could be used making it more pretty in different browsers,
    making the color wheels work in IE, and general IE testing.

---
+++ examples/playground/playground.js
@@ -0,0 +1,413 @@
+/*
+    This code is a direct port to jQuery originally written in Prototype 
+    by Fabien Menager for the flotr project.
+
+    TODO:
+    -add more plugins (Ryley won't be doing this)
+    -handling of colors is crap, doesn't auto-update, messes up IE, etc.  farbtastic is not a winner.  
+    -oh, and it defaults to grey (see farbtastic code)... and freaks out if you default it to blank
+        which flot needs to use its default colors...
+*/
+function insertElement(object, title, name, container){
+    var input;
+    switch (object.type) {
+        case String:
+            if (!object.values) {
+                input = $(document.createElement('input'));
+                input.val(object.def);
+            }
+            else {
+                input = $(document.createElement('select'));
+                $.each(object.values,function(i,v){
+                    input.append('<option value="'+v+'"'+((object.def == v)?'selected="selected"':'')+'>'+
+                        ((object.labels && object.labels[i])?object.labels[i]:v)+'</option>');
+                });
+            }
+            break;
+        case Function:
+            input = $(document.createElement('textarea'));
+            input.attr('cols',30).attr('rows',3).val(object.def.toString());
+            break;
+        case Number:
+            input = $(document.createElement('input'));
+            input.attr('type','text').attr('size',2).val(object.def);
+            break;
+        case Boolean:
+            input = $(document.createElement('input'));
+            input.attr('type','checkbox').attr('checked',object.def);
+            break;
+        case Color:
+        default:
+            //this is a horrible failure, but basically, Color is our only non-builtin type, and it fails to copy
+            //to axes due to the way extend works.
+            object.type = Color;
+            input = $(document.createElement('input'));
+            input.attr('type','text').attr('size',8).val(object.def);
+            break;
+    }
+    input.attr('name',name);
+    input.attr('id',name);
+    input.attr('disabled',true);
+    var activator = $(document.createElement('input'));
+    activator.attr('type','checkbox').css('float','left').click(function(){
+        input.attr('disabled',!this.checked);
+        input.parent().toggleClass('active');
+    });
+    var line = $(document.createElement('div')).addClass('line removeable').append(activator);
+
+    var label = $(document.createElement('label')).append('<span>'+title+'</span>').append(input);
+
+    line.append(label);
+    if (object.type == Color){
+        var wheel = $(document.createElement('div')).addClass('wheel removeable');
+        line.append(wheel);
+        $(wheel).farbtastic(input).hide();
+        input.focus(function(){
+            $(wheel).show();
+        }).blur(function(){
+            $(wheel).hide();
+        });
+    } 
+    container.append(line);
+}
+
+function buildForm(specs, form){
+  var opt;
+    $.each(specs,function(key,o){
+        if (o.def == undefined) {
+            opt = o._options;
+            
+            if (!opt.inherited && form.attr('name') != 'global') { return true; }
+            if ((opt.axis || opt.seriesOnly) && form.attr('name') == 'global') { return true; }
+            var legend = $(document.createElement('legend')).html('<a href="javascript:;">' + opt.title + '</a>'),
+                fieldset = $(document.createElement('legend')).addClass('removeable').append(legend),
+                container = $(document.createElement('span'));
+                
+            fieldset.append(container);
+            form.append(fieldset);
+            legend.click(function(){ container.toggle();});
+            
+            if (opt.collapsed) container.hide();
+    
+            $.each(o,function(k,v){
+                if (v && k != '_options'){
+                    insertElement(v, k, key + '-' + k, container);
+                }
+            });
+        }
+        else 
+            if (form.attr('name') == 'global') {
+                insertElement(o, key, key, form);
+            }
+    });
+}
+
+function input(){
+  var options = {}, 
+      series = [];
+      
+  eval($('#input-code').val());
+  
+  $('.removeable').remove();
+  
+    buildForm(specs, $(document.forms.global));
+    
+    $(document.forms.global).find('input,select,textarea').each(function(e){
+        $(e).change(draw);
+    });
+  
+  buildDataForms();
+    
+  $('#tabs').tabs();
+  $('#tabs-io').tabs();
+}
+
+function draw(){
+    var globalOptions = formToOptions($(document.forms.global)), series = [];
+    
+    $.each(data,function(i,serie){
+        $.extend(serie, formToOptions($(document.forms[serie.id + '-options'])));
+        inputsToData($(document.forms[serie.id + '-data']), serie);
+        inputsToSerie(document.forms[serie.id + '-serie'], serie);
+        
+        if (!serie.hide) 
+            series.push(serie);
+    });
+    
+    var f = $.plot($('#container'), series, globalOptions);
+    
+    writeCode($('#output-code'), series, globalOptions);
+}
+
+function writeCode(container, series, options){
+    container.html('options = <span style="white-space: normal">' + $.toJSON(options) + ';</span><br /><br />series = ' + $.toJSON(series) + ';');
+}
+
+function getOptionValue(element){
+    if (element.nodeName.match(/^textarea$/i)) {
+        eval('window.__FlotFunc = ' + element.value);
+        window.__FlotFunc.toJSON = function(){ return element.value.replace(/\n/g,'<br/>'); } ;
+        return window.__FlotFunc;
+    }
+    else 
+        if (element.nodeName.match(/^input$/i)) {
+            if (element.type == 'text') {
+                if (element.size == 2) 
+                    return element.value === '' ? null : parseFloat(element.value);
+                else 
+                    return element.value;
+            }
+            else 
+                if (element.type == 'checkbox') {
+                    return !!element.checked;
+                }
+        }
+        else 
+            if (element.nodeName.match(/^select$/i)) {
+                return element.options[element.selectedIndex].value;
+            }
+}
+
+function formToOptions(form){
+    var options = {}, parts, value,matches;
+    form.find('input,select,textarea').each(function(i,e){
+        if (e.disabled || !e.name) { return true; }
+
+        parts = e.name.split('-');
+        value = $(e).val();
+        
+        if ((matches = parts[0].match(/(x|y)axis(\d*)/))) {
+            var axis = 0;
+            if (matches[2]){
+                axis = matches[2]-1;
+            }
+            if (matches[1] == 'x' && !options.xaxes)
+                options.xaxes = [{}];
+            if (matches[1] == 'y' && !options.yaxes)
+                options.yaxes = [{}];
+            if (matches[1] == 'x' && !options.xaxes[axis])
+                options.xaxes[axis] = {};
+            if (matches[1] == 'y' && !options.yaxes[axis])
+                options.yaxes[axis] = {};
+            options[matches[1]+'axes'][axis][parts[1]] = getOptionValue(e);
+        } else {
+            if (parts[1]) {
+                options[parts[0]] = options[parts[0]] || {};
+                options[parts[0]][parts[1]] = getOptionValue(e);
+            }
+            else {
+                options[parts[0]] = getOptionValue(e);
+            }
+        }
+    });
+    return options;
+}
+
+
+function getDataInputs(data, n){
+    n = n || 5;
+    
+    var i, str = '<div class="data-entry">', type = ['x', 'y', 'a', 'b', 'c'];
+    for (i = 0; i < n; i++) {
+        str += '<input type="text" size="2" value="' + (data ? (data[i] || 0) : '') + '" name="' + type[i] + '" class="level-' + i + '" ' + (i > 5 ? 'disabled="disabled"' : '') + '/>';
+    }
+    str += '<a href="javascript:;" onclick="$(this).parent().remove()">-</a></div>';
+    return str;
+}
+
+function inputsToData(form, serie){
+    var i, data = {x: [], y: [], a: [], b: [], c: []};
+    
+    $.each(data,function(k,v){
+        form.find('input[name="'+k+'"]').each(function(i,e){
+            if (!e.disabled && e.value) {
+                data[k].push(parseFloat(e.value));
+            }
+        });
+    });
+    serie.data = [];
+    for (i = 0; i < data.x.length; i++) {
+        serie.data.push([data.x[i], data.y[i]]);
+    }
+}
+
+function inputsToSerie(form, serie){
+    var els = form.elements;
+    serie.label = els.label.value;
+    serie.color = els.color.value?els.color.value:null;//default to nothing, which will let flot fill it
+    serie.xaxis = parseInt($(els).find('select[name="xaxis"] option:selected').val());
+    serie.yaxis = parseInt($(els).find('select[name="yaxis"] option:selected').val());
+    serie.hide = !els.show.checked;
+}
+
+//by default there is only x/y axes, allow them to add an arbitrary number of axes instead
+function addAxesButtons(){
+    var form = $(document.forms.global);
+    var fsX = $(document.createElement('fieldset')).append('<legend>Axes</legend>');
+
+    $(document.createElement('button')).text('Add X Axis').appendTo(fsX).click(function(){
+        addAxisData(form,fsX,'x',xAxes,specs.xaxis);
+        return false;
+    });
+    $(document.createElement('button')).text('Add Y Axis').appendTo(fsX).click(function(){
+        addAxisData(form,fsX,'y',yAxes,specs.yaxis);
+        return false;
+    });
+
+    form.prepend(fsX);
+    addAxisForm(form,specs.xaxis,'xaxis');
+    addAxisForm(form,specs.yaxis,'yaxis');
+}
+
+function addAxisData(form,fsX,type,axes,spec){
+    var index = axes.length+1;
+    var newAxis = $.extend(true,{},spec,{ 
+        _options: {
+            title: spec._options.title+' #'+index
+        }
+    });
+    axes.push(newAxis);
+    addAxisForm(form,newAxis,type+'axis'+index);
+    $('select[name="'+type+'axis"]').append('<option value="'+index+'">'+index+'</option>');
+    if (axes.length == 2){
+        $(document.createElement('button')).attr('id',type+'axis-removal').appendTo(fsX).click(function(){
+            $('select[name="'+type+'axis"] option[value="'+xAxes.length+'"]').remove();
+            axes.splice(axes.length-1,1);
+            $('.'+type+'axis:eq('+axes.length+')').remove();
+            if (axes.length == 1)
+                $(this).remove();
+            else
+                $(this).text('Remove '+type+' axis '+xAxes.length);
+            return false;
+        });
+    }
+    $('#'+type+'axis-removal').text('Remove '+type+' axis '+index);
+    return false;
+}
+
+//add the actual form to the global options
+function addAxisForm(form,axis,name){
+    var o = axis;
+    var opt = o._options;
+    
+    var legend = $(document.createElement('legend')).html('<a href="javascript:;">' + opt.title + '</a>'),
+        fieldset = $(document.createElement('legend')).addClass(name+' removeable').append(legend),
+        container = $(document.createElement('span'));
+        
+    fieldset.append(container);
+    form.append(fieldset);
+    legend.click(function(){ container.toggle();});
+    
+    if (opt.collapsed) container.hide();
+
+    $.each(o,function(k,v){
+        if (v && k != '_options'){
+            insertElement(v, k, name + '-' + k, container);
+        }
+    });
+
+    form.find('input,select,textarea').each(function(e){
+        $(e).change(draw);
+    });
+}
+
+function buildDataForms(){
+  var i, k, a, values, entries, fieldsetData, fieldsetSerie, formData, formOptions, formSerie;
+    $.each(data,function(i,serie){ 
+        fieldsetData = $(document.createElement('fieldset')).append('<legend>Data</legend>');
+        fieldsetSerie = $(document.createElement('fieldset')).append('<legend>Options</legend>');
+    
+        formData = $(document.createElement('form')).attr('name',serie.id + '-data').append(fieldsetData);
+    
+        formOptions = $(document.createElement('form')).attr('name',serie.id + '-options').css('clear','both');
+        
+        formSerie = $(document.createElement('form')).attr('name',serie.id + '-serie').css('clear','both').addClass('serie-options').append(fieldsetSerie);
+        
+        var container = $(document.createElement('div')).attr('id',serie.id).addClass('removeable').append(formSerie).append(formData).append(formOptions).appendTo('#configurator');
+        
+        serie.toJSON = function(){
+            values = [];
+            for (k in serie) {
+                if (k != 'toJSON' && k != 'id' && k != 'hide' && this[k] !== null) {
+                    entries = [];
+                    if ($.isArray(this[k])) {
+                        a = this[k];
+                        for (i = 0; i < a.length; i++) {
+                            entries.push(JSON.stringify(a[i]));
+                        }
+                        str = 'data:<br /> [' + entries.join(',<br />') + ']';
+                    }
+                    else {
+                        str = k + ': ' + JSON.stringify(this[k]);
+                    }
+                    values.push(str);
+                }
+            }
+            return '<br />{' + values.join(', ') + '}<br />';
+        }
+        
+        for (i = 0; i < serie.data.length; i++) {
+            fieldsetData.append(getDataInputs(serie.data[i],2));
+        }
+        fieldsetSerie.append('<div class="axis-select">' +
+        'x Axis :' +
+        '<label><select name="xaxis"><option value="1">1</option></select></label>' +
+        '<br />y Axis :' +
+        '<label><select name="yaxis"><option value="1">1</option></select></label></div>' +
+        '<input type="text" value="' + serie.label + '" name="label" size="14" /> '+
+        '<input type="text" value="' + (serie.color?serie.color:'') + '" name="color" size="8" /><div class="wheel"></div><br />' +
+        '<label><input type="checkbox" checked="checked" name="show" /> Show</label>');
+        fieldsetData.append('<a href="javascript:;" class="plus" onclick="$(this).before(getDataInputs(null,2))">+</a>');
+        
+        var xaxis = fieldsetSerie.find('select[name="xaxis"]');
+        var yaxis = fieldsetSerie.find('select[name="yaxis"]');
+        var wheel = fieldsetSerie.find('div.wheel');
+        var input = fieldsetSerie.find('input[name="color"]');
+        $(wheel).farbtastic(input).hide();
+        input.focus(function(){
+            $(wheel).show();
+        }).blur(function(){
+            $(wheel).hide();
+        });
+
+        //add appropriate axes to each select
+        for (var i = 1;i < xAxes.length;i++){
+            xaxis.append('<option value="'+(i+1)+'">'+(i+1)+'</option>');
+        }
+        for (var i = 1;i < yAxes.length;i++){
+            yaxis.append('<option value="'+(i+1)+'">'+(i+1)+'</option>');
+        }
+
+        buildForm(specs, formOptions);
+    
+        formOptions.find('input,select,textarea').each(function(i,e){
+            $(e).change(draw);
+        });
+        
+        formSerie.find('input,select,textarea').each(function(i,e){
+            $(e).change(draw);
+        });
+        
+        // Add the data tab
+        $('#tabs ul').append('<li class="removeable"><a href="#' + serie.id + '">' + serie.label + '</a></li>');
+    });
+}
+
+function main(){
+    buildForm(specs, $(document.forms.global));
+    addAxesButtons();
+    
+    $(document.forms.global).find('input,select,textarea').each(function(i,e){
+        $(e).change(draw);
+    });
+
+  buildDataForms();
+    
+    $('#tabs').tabs();
+    $('#tabs-io').tabs();
+    draw();
+}
+
+$(document).ready(function(){
+    main();
+});
---
+++ examples/playground/specs.js
@@ -0,0 +1,554 @@
+var Color = Color || {};
+
+
+var specs = {
+    series: {
+		_options: {
+			title: "Series",
+			collapsed: true
+		},
+        stack: {
+            type: Boolean,
+            def: false
+        },
+    },
+	legend: {
+		_options: {
+			title: "Legend",
+			collapsed: true
+		},
+		show: {
+			type: Boolean,
+			def: true
+		},
+		labelFormatter: {
+			type: Function,
+			def: function (x) { return x; }
+		},
+		labelBoxBorderColor: {
+			type: Color,
+			def: '#CCCCCC'
+		},
+		container: null,
+		position: {
+			type: String,
+			def: 'ne',
+			values: ['se', 'ne', 'sw', 'nw'],
+			labels: ['Bottom right', 'Top right', 'Bottom left', 'Top left']
+		},
+		margin: {
+			type: Number,
+			def: 5
+		},
+        backgroundColor: {
+			type: Color,
+			def: null
+		},
+		backgroundOpacity: {
+			type: Number,
+			def: 0.85
+		}
+	},
+	xaxis: {
+		_options: {
+			title: "x Axis",
+			collapsed: true,
+            axis: true
+		},
+		ticks: null,
+		labelAngle: {
+			type: Number,
+			def: 0
+		},
+		position: {
+			type: String,
+			def: 'bottom',
+			values: ['bottom', 'top'],
+			labels: ['Bottom', 'Top']
+		},
+		tickFormatter: {
+			type: Function,
+			def: function (v,axis) { return ''+v; }
+		},
+		tickDecimals: {
+			type: Number,
+			def: null
+		},
+		min: {
+			type: Number,
+			def: null
+		},
+		max: {
+			type: Number,
+			def: null
+		},
+		autoscaleMargin: {
+			type: Number,
+			def: 0
+		},
+        color: {
+			type: Color,
+			def: null
+		},
+        tickLength: {
+            type: Number,
+            def:null
+        },
+		mode: {
+			type: String,
+			def: 'normal',
+			values: ['normal', 'time'],
+			labels: ['Normal', 'Time']
+		},
+		timeFormat: {
+			type: String,
+			def: null
+		},
+		transform : {
+			type: Function,
+			def: function (v) { return v; }
+		},
+		inverseTransform : {
+			type: Function,
+			def: function (v) { return v; }
+		}
+	},
+	yaxis: {
+		_options: {
+			title: "y Axis",
+			collapsed: true,
+            axis: true
+		},
+		ticks: null,
+		labelAngle: {
+			type: Number,
+			def: 0
+		},
+		position: {
+			type: String,
+			def: 'left',
+			values: ['left', 'right'],
+			labels: ['Left', 'Right']
+        },
+		tickFormatter: {
+			type: Function,
+			def: function (v,axis) { return ''+v; }
+		},
+		tickDecimals: {
+			type: Number,
+			def: null
+		},
+		min: {
+			type: Number,
+			def: null
+		},
+		max: {
+			type: Number,
+			def: null
+		},
+		autoscaleMargin: {
+			type: Number,
+			def: 0
+		},
+        color: {
+			type: Color,
+			def: null
+		}
+	},
+	points: {
+		_options: {
+			title: "Points",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		radius: {
+			type: Number,
+			def: 3
+		},
+		lineWidth: {
+			type: Number,
+			def: 2
+		},
+		fill: {
+			type: Boolean,
+			def: true
+		},
+        fillColor: {
+			type: Color,
+			def: '#FFFFFF'
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		}
+	},
+	lines: {
+		_options: {
+			title: "Line chart",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		lineWidth: {
+			type: Number,
+			def: 2
+		},
+		fill: {
+			type: Boolean,
+			def: false
+		},
+        fillColor: {
+			type: Color,
+			def: null
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		}
+	},
+	bars: {
+		_options: {
+			title: "Bar chart",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		lineWidth: {
+			type: Number,
+			def: 2
+		},
+		barWidth: {
+			type: Number,
+			def: 1
+		},
+		fill: {
+			type: Boolean,
+			def: true
+		},
+        fillColor: {
+			type: Color,
+			def: null
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		},
+        horizontal: {
+            type: Boolean,
+            def: false
+        },
+		align : {
+			type: String,
+			def: 'left',
+			values: ['left', 'center'],
+			labels: ['Left', 'Center']
+		}
+	},
+	/*pie: {
+		_options: {
+			title: "Pie chart",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		lineWidth: {
+			type: Number,
+			def: 1
+		},
+		fill: {
+			type: Boolean,
+			def: true
+		},
+		fillColor: {
+			type: Color,
+			def: null
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.6
+		},
+		explode: {
+			type: Number,
+			def: 6
+		},
+		sizeRatio: {
+			type: Number,
+			def: 0.6
+		},
+		startAngle: {
+			type: Number,
+			def: Math.PI / 4
+		},
+		labelFormatter: {
+			type: Function,
+			def: null
+			//def: Flotr.defaultPieLabelFormatter
+		}
+	},
+	markers: {
+		_options: {
+			title: "Markers",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		lineWidth: {
+			type: Number,
+			def: 1
+		},
+		fill: {
+			type: Boolean,
+			def: false
+		},
+		fillColor: {
+			type: Color,
+			def: "#FFFFFF"
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		},
+		stroke: {
+			type: Boolean,
+			def: false
+		},
+		position: {
+			type: String,
+			def: 'ct',
+			values: ['lt', 'ct', 'rt', 'lm', 'cm', 'rm', 'lb', 'cb', 'rb'],
+			labels: ['Left top', 'Center top', 'Right top', 'Left middle', 'Center middle', 'Right middle', 'Left bottom', 'Center bottom', 'Right bottom']
+		},
+		labelFormatter: {
+			type: Function,
+			def: null
+			//def: Flotr.defaultMarkerFormatter
+		}
+	},
+	bubbles: {
+		_options: {
+			title: "Bubbles",
+			collapsed: true,
+			inherited: true
+		},
+		show: {
+			type: Boolean,
+			def: false
+		},
+		lineWidth: {
+			type: Number,
+			def: 2
+		},
+		fill: {
+			type: Boolean,
+			def: true
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		},
+		baseRadius: {
+			type: Number,
+			def: 2
+		}
+	},*/
+	grid: {
+		_options: {
+			title: "Grid",
+			collapsed: true
+		},
+        show: {
+			type: Boolean,
+			def: true
+        },
+		color: {
+			type: Color,
+			def: '#545454'
+		},
+		backgroundColor: {
+			type: Color,
+			def: null
+		},
+		tickColor: {
+			type: Color,
+			def: '#DDDDDD'
+		},
+		labelMargin: {
+			type: Number,
+			def: 3
+		},
+		aboveData: {
+			type: Boolean,
+			def: false 
+		}
+	},
+	/*selection: {
+		_options: {
+			title: "Selection",
+			collapsed: true
+		},
+		mode: {
+			type: String,
+			def: null,
+			values: ['x', 'y', 'xy']
+		},
+		color: {
+			type: Color,
+			def: '#B6D9FF'
+		},
+		fps: {
+			type: Number,
+			def: 20
+		}
+	},
+	crosshair: {
+		_options: {
+			title: "Crosshair",
+			collapsed: true
+		},
+		mode: {
+			type: String,
+			def: null,
+			values: ['x', 'y', 'xy']
+		},
+		color: {
+			type: Color,
+			def: '#FF0000'
+		},
+		hideCursor: {
+			type: Boolean,
+			def: true
+		}
+	},
+	mouse: {
+		_options: {
+			title: "Mouse",
+			collapsed: true,
+			inherited: true
+		},
+		track: {
+			type: Boolean,
+			def: false
+		},
+		position: {
+			type: String,
+			def: 'se',
+			values: ['se', 'ne', 'sw', 'nw'],
+			labels: ['Bottom right', 'Top right', 'Bottom left', 'Top left']
+		},
+		relative: {
+			type: Boolean,
+			def: false
+		},
+		trackFormatter: {
+			type: Function,
+			def: null
+			//def: Flotr.defaultTrackFormatter
+		},
+		margin: {
+			type: Number,
+			def: 5
+		},
+		lineColor: {
+			type: Color,
+			def: '#FF3F19'
+		},
+		trackDecimals: {
+			type: Number,
+			def: 1
+		},
+		sensibility: {
+			type: Number,
+			def: 2
+		},
+		radius: {
+			type: Number,
+			def: 3
+		},
+		fillColor: {
+			type: Color,
+			def: null
+		},
+		fillOpacity: {
+			type: Number,
+			def: 0.4
+		}
+	},*/
+    threshold: {
+		_options: {
+			title: "Threshold",
+			collapsed: true,
+            seriesOnly: true,
+            inherited: true
+		},
+		below : {
+			type: Number,
+			def: 5  
+		},
+		color : {
+			type: Color,
+			def: null
+		},
+
+    }
+};
+
+var xAxes = [ specs.xaxis ];
+var yAxes = [ specs.yaxis ];
+
+var data = [{
+	label: 'Serie 1',
+	hide: false,
+	id: 'serie-0',
+    color: null,
+	data: [[0, 3.206, 3.474, 2.212, 2.698], [1, 2.698, 3.368, 2.59, 2.926], [2, 2.926, 3.328, 2.9, 3.258], [3, 3.258, 3.559, 2.802, 3.171], [4, 3.171, 4.14, 2.995, 3.473], [5, 3.473, 4.429, 3.268, 3.913], [6, 3.913, 4.745, 3.594, 3.905], [7, 3.905, 4.29, 3.273, 3.522], [8, 3.522, 3.732, 3.272, 3.62], [9, 3.62, 4.006, 2.888, 3.225], [10, 3.225, 3.774, 2.807, 3.182]]
+}, {
+	label: 'Serie 2',
+	hide: false,
+	id: 'serie-1',
+    color: null,
+	data: [[0, 2.206, 4.047, 2.493, 4.023], [1, 4.023, 4.689, 3.058, 3.872], [2, 3.872, 4.371, 3.065, 3.924], [3, 3.924, 4.344, 3.042, 3.21], [4, 3.21, 3.741, 2.795, 2.855], [5, 2.855, 3.668, 2.807, 3.648], [6, 3.648, 3.713, 3.249, 3.308], [7, 3.308, 4.055, 2.389, 3.663], [8, 3.663, 4.392, 3.235, 3.592], [9, 3.592, 4.584, 2.857, 4.235], [10, 4.235, 5.128, 4.023, 4.138]]
+}, {
+	label: 'Serie 3',
+	hide: false,
+	id: 'serie-2',
+    color: null,
+	data: [[0, 4.206, 3.266, 3.142, 3.146], [1, 3.146, 3.551, 2.524, 3.289], [2, 3.289, 4.288, 2.493, 2.999], [3, 2.999, 3.053, 2.211, 2.225], [4, 2.225, 2.418, 2.222, 2.242], [5, 2.242, 2.795, 1.388, 2.303], [6, 2.303, 2.846, 1.764, 2.731], [7, 2.731, 3.263, 2.098, 2.609], [8, 2.609, 2.917, 2.077, 2.735], [9, 2.735, 2.773, 2.152, 2.771], [10, 2.771, 2.849, 1.823, 2.335]]
+}, {
+	label: 'Serie 4',
+	hide: false,
+	id: 'serie-3',
+    color: null,
+	data: [[0, 3.206, 3.96, 2.944, 3.77], [1, 3.77, 4.408, 3.215, 3.984], [2, 3.984, 4.466, 3.832, 4.1], [3, 4.1, 4.914, 4.073, 4.466], [4, 4.466, 4.498, 3.664, 3.862], [5, 3.862, 4.089, 3.599, 3.635], [6, 3.635, 4.331, 3.052, 4.051], [7, 4.051, 4.427, 3.503, 4.402], [8, 4.402, 4.477, 3.534, 3.753], [9, 3.753, 3.89, 2.996, 3.291], [10, 3.291, 3.679, 3.187, 3.255]]
+}, {
+	label: 'Serie 5',
+	hide: false,
+	id: 'serie-4',
+    color: null,
+	data: [[0, 2.706, 4.177, 3.012, 4.084], [1, 4.084, 5.039, 3.831, 4.18], [2, 4.18, 4.688, 3.226, 3.399], [3, 3.399, 4.247, 2.644, 3.675], [4, 3.675, 4.377, 2.908, 3.705], [5, 3.705, 4.38, 2.916, 3.067], [6, 3.067, 3.528, 2.673, 3.007], [7, 3.007, 3.833, 2.68, 3.381], [8, 3.381, 3.529, 3.276, 3.446], [9, 3.446, 3.49, 3.403, 3.486], [10, 3.486, 3.663, 2.865, 3.594]]
+}];
+
+for (var i = 0; i < data.length; i++) {
+	var d = data[i].data;
+	for (var j = 0; j < d.length; j++) {
+		var l = d[j];
+		for (var k = 0; k < l.length; k++) {
+			l[k] = parseFloat(l[k].toFixed(3));
+		}
+	}
+}
